80ea84c3197886bf24b80962764c8b873da65bf1
Ritaja <ritaja.sengupta@gmail.com>
2014-06-12 15:56:11 +0200

    Added document

--- bin/unsupported/stop_demo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for host in Administrator@10.0.0.97 Administrator@10.0.0.97 Administrator@10.0.0.97 Administrator@10.0.0.98 Administrator@10.0.0.98 Administrator@10.0.0.97 Administrator@10.0.0.97 Administrator@10.0.0.97 Administrator@10.0.0.97 Administrator@10.0.0.98 Administrator@10.0.0.98 Administrator@10.0.0.98
-do
-ssh $host 'killall java; killall rmiregistry'
-done
-
