7c450156f5aac9b5f14a8eaab0675c1015602a4d
sayanh <sayanh>
2014-08-13 14:18:00 +0200

    Adding shell for new broker creation

--- etc/scripts/instantiate_server.sh
+++ etc/scripts/instantiate_server.sh
@@ -2,7 +2,7 @@
 
 
 ## Trim the input in the instantiate_server.sh and then compare the results to null or empty.
-
+# Run this from console: sh instantiate_server.sh "socket://192.168.1.35:1100/BrokerA" "socket://localhost:9997/newbrokerC" "socket://192.168.1.35:1101/BrokerB","socket://192.168.1.35:1100/BrokerA"
 if [ ! "$1" -o ! "$2" -o ! "$3" ]
 then
 	echo "format: sh instantiate.sh <overloadBrkUri> <uriLoadAcceptingBrk> <neighbors>"
@@ -15,7 +15,11 @@ overloadBrkUri=$1
 uriLoadAcceptingBrk=$2
 neighbors=$3
 searchstr="localhost"
-
+echo "Current directory=" 
+pwd
+PADRES_HOME="$(cd $(dirname "$0")/../.. && pwd)"
+export PADRES_HOME
+echo "####PADRES_HOME=" $PADRES_HOME "   "
  
 typeOnConn=""
 echo "SSH client starts.............."
@@ -28,12 +32,13 @@ echo "Neighbors are:" $neighbors
 
 if test "${uriLoadAcceptingBrk#*$searchstr}" != "$uriLoadAcceptingBrk"
 then 
-	echo "Connecting to the new broker in the localhost"
+	echo -e "###Connecting to the new broker in the localhost####\n"			
 	pwd
-	cd ../../
-	java ca.utoronto.msrg.padres.broker.brokercore.BrokerCore -uri $uriLoadAcceptingBrk -n $neighbors -ovl overloadBrkUri loadbalance 
-	
+	#java ca.utoronto.msrg.padres.broker.brokercore.BrokerCore -uri $uriLoadAcceptingBrk -n $neighbors -ovl overloadBrkUri loadbalance
+#	sh $PADRES_HOME/etc/scripts/startnewbroker.sh -uri $uriLoadAcceptingBrk -n $neighbors -ovl overloadBrkUri loadbalance	
+	bash $PADRES_HOME/etc/scripts/startnewbroker -uri $uriLoadAcceptingBrk -n $neighbors -ovl overloadBrkUri loadbalance	
 else
+	# ssh to other server yet to be done
 	echo "Connecting to the new broker in a remote machine"
 	ssh $username@$hostname java -jar ../../ms3-server.jar $port INFO 
 fi
